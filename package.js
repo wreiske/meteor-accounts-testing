485556268ff635ac066dae2f8b9e651bbf98a243
Andrew Mao <mizzao@gmail.com>
2013-07-30 18:56:23 -0400

    update packaging for 0.6.5

--- package.js
+++ package.js
@@ -2,9 +2,12 @@ Package.describe({
     summary: "Accounts drop-in for testing that allows logging in without a password."
 });
 
+var both = ['client','server'];
+
 Package.on_use( function(api) {
-    api.use('accounts-base', ['client','server']);
-    api.use('coffeescript', 'server');
+    api.use('accounts-base', both);
+    api.use('coffeescript', both);
+
     api.add_files('accounts_testing_server.coffee', 'server');
     api.add_files('accounts_testing_client.coffee', 'client');
 });
