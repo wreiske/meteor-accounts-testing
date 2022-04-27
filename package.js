e34fb3c6b0e20149d58a215d977f0733708e4c1f
Will Reiske <wreiske@mieweb.com>
2022-04-27 13:52:36 -0700

    Changes for new meteor packages API

--- package.js
+++ package.js
@@ -1,11 +1,14 @@
 Package.describe({
-    summary: "Accounts drop-in for testing that allows logging in without a password."
+    name: 'communitypackages:accounts-testing',
+    summary: 'Accounts drop-in for testing that allows logging in without a password.',
+    version: '0.1.1',
+    git: 'https://github.com/Meteor-Community-Packages/meteor-accounts-testing.git'
 });
 
-Package.on_use( function(api) {
+Package.onUse((api) => {
     api.imply(['accounts-base', 'accounts-ui']);
     api.use(['accounts-base', 'coffeescript']);
 
-    api.add_files('accounts_testing_server.coffee', 'server');
-    api.add_files('accounts_testing_client.coffee', 'client');
+    api.addFiles('accounts_testing_server.coffee', 'server');
+    api.addFiles('accounts_testing_client.coffee', 'client');
 });
